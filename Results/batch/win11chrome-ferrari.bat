73cd03378a89d2b4cc3ff5bf11edecde627e70da
Rob <robertgisbey@gmail.com>
2026-03-30 18:17:46 +0100

    Update batch script for win11chrome-ferrari

--- Results/batch/win11chrome-ferrari.bat
+++ Results/batch/win11chrome-ferrari.bat
@@ -1,4 +1,4 @@
 @echo off
 cd C:\development\robot-scripts\amazon
-call robot -d results/batch/win11chrome -v BROWSER:chrome -v SEARCH_TERM:"LEGO Speed Champions Ferrari SF-24" Tests/amazon.robot
+call robot -d results/batch/win11chrome-ferrari -v BROWSER_TYPE:chrome -v SEARCH_TERM:"LEGO Speed Champions Ferrari SF-24" Tests/amazon.robot
 
