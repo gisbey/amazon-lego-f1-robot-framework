ba6207bfac1808a0eb50232f9268a3f5bf25416f
Rob <robertgisbey@gmail.com>
2026-03-30 18:17:11 +0100

    Update batch file to use BROWSER_TYPE variable

--- Results/batch/win11firefox-mclaren.bat
+++ Results/batch/win11firefox-mclaren.bat
@@ -1,4 +1,4 @@
 @echo off
 cd C:\development\robot-scripts\amazon
-call robot -d results/batch/win11firefox-mclaren -v BROWSER:firefox -v SEARCH_TERM:"LEGO Speed Champions McLaren F1 Team MCL38" Tests/amazon.robot
+call robot -d results/batch/win11firefox-mclaren -v BROWSER_TYPE:firefox -v SEARCH_TERM:"LEGO Speed Champions McLaren F1 Team MCL38" Tests/amazon.robot
 
